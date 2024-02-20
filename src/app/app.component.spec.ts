e6e6faad9069b685d1b859b2fcbe51366c1d46b4
Daniel Molnar <molnard0412@gmail.com>
2024-02-19 19:25:59 -0500

    another round of rename to 'perke'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ProjName' title`, () => {
+  it(`should have the 'perke' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProjName');
+    expect(app.title).toEqual('perke');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ProjName');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, perke');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Perke';
+  myNextProject = 'PERKE';
+
+  showPurpleBoxShadow = true;
+  get boxShadow() {
+    if (this.showPurpleBoxShadow) return 'purple-box-shadow';
+
+    return 'pink-box-shadow';
+  }
 }
